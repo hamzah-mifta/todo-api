90b9bda4b8256457f7b3bd6503da7c2151e4d8e0
hamzah-mifta <hamzmk09@gmail.com>
2021-12-29 21:59:13 +0700

    Add npm compression

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const responseFormat = require('./utils/response');
 const router = require('./routes');
 
 const app = express();
 
+app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
